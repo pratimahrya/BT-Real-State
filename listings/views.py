973e9fd5152fdd06aceb4f1ea4d4681fdbd99cb7
Pratima Acharya <pratima.acharya@tekvortex.local@ASSET056.tekvortex.local>
2024-08-12 14:41:36 +0545

    css changes

--- listings/views.py
+++ listings/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'listings/listings.html')
+    return render(request, 'listings/listings.html', {
+        
+    })
 
 def listing(request):
     return render(request, 'listings/listing.html')
